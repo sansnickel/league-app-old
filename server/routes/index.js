2dbf856779171a725fc5e8bb03006d5af848e5f0
sansnickel <nleungky@gmail.com>
2018-07-12 12:17:38 -0700

    able to get account ids

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,53 @@
 var express = require('express');
+var app = express()
+
 var router = express.Router();
 
-router.get('/message', function(req, res, next) {
-  res.json('Welcome to React');
+var https = require('https');
+
+router.get('/hi', function(req, res, next) {
+	console.log(req.query.summonername)
+	var options = {
+		host: 'na1.api.riotgames.com',
+		path: '/lol/summoner/v3/summoners/by-name/' + req.query.summonername,
+		headers: {
+			'X-Riot-Token': 'RGAPI-2b97885a-28c1-4cdf-bd28-82f4af937aff'
+		}
+	};
+
+	var req = https.get(options, function(response) {
+		console.log('STATUS: ' + response.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(response.headers));
+
+// Buffer the body entirely for processing as a whole.
+var bodyChunks = [];
+response.on('data', function(chunk) {
+// You can process streamed parts here...
+bodyChunks.push(chunk);
+}).on('end', function() {
+	var body = Buffer.concat(bodyChunks);
+	console.log('BODY: ' + body);
+	console.log(JSON.parse(body).accountId);
+
+
+	res.json(JSON.parse(body).accountId)
+// ...and/or process the entire body here.
+})
 });
+
+	req.on('error', function(e) {
+		console.log('ERROR: ' + e.message);
+	});
+
+
+})
+
+router.get('/message', function(req, res, next) {
+	console.log('hello')
+	res.json('Welcome To React')
+})
+
+
+
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -5,14 +5,61 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = { message: '' };
+    this.state = { message: '',
+                   summonername: '', 
+                   accountid: '0' }
+    this.updateState = this.updateState.bind(this);
+  };
+  
+  getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+  updateState(e) {
+    this.setState({ summonername: e.target.value })
+  }
+ 
+  async getAccountId() {
+
+    const response = await fetch('/api/hi/?summonername=' + this.state.summonername);
+    const json = await response.json();
+    this.setState({accountid: json});
+/*
+    fetch('/api/hi')
+     .then(response=>response.json())
+     .then(json=> this.setState({ accountid: json}))*/
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+
+    const response = await fetch('/api/message');
+    const json = await response.json();
+    this.setState({message: json});
+
+/*
     fetch('/api/message')
       .then(response => response.json())
-      .then(json => this.setState({ message: json }));
+      .then(json => this.setState({ message: json }))*/
+
+    this.setState({ summonername: this.getParameterByName('summonername')}, this.getAccountId);
+   
+
   }
+
+  async asyncData () {
+
+
+    
+ 
+
+  }
+
+  
 
   render() {
     return (
@@ -24,9 +71,25 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+        <form onSubmit={this.updateState}>
+          <label htmlFor="summonername">Enter Summoner Name</label>
+            <input id="summonername" name="summonername" type="text"/>        
+          <button>Search</button>
+        </form>
+        <h4>{this.state.summonername}{this.state.accountid}</h4>
+        </div>
       </div>
+
+
+
     );
   }
+
+
+
 }
+
+
 
 export default App;
